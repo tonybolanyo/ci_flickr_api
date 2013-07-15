39279630b2acf487ecca29fc9e6a870ee9cb02b4
Tony G. Bolaño <tonybolanyo@gmail.com>
2013-07-15 12:35:46 +0200

    Spanish translation created

---
+++ language/spanish/flickr_api_lang.php
@@ -0,0 +1,34 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter Flickr API
+ * 
+ * CodeIgniter Flickr API is a CodeIgniter library which gives access to make calls to Flickr's API.
+ * 
+ * @package  	CodeIgniter Flickr API
+ * @author		LMB^Box (Thomas Montague)
+ * @translate   Tony G. Bolaño
+ * @copyright	Copyright (c) 2009 - 2011, LMB^Box
+ * @license		GNU Lesser General Public License (http://www.gnu.org/copyleft/lgpl.html)
+ * @link		http://lmbbox.com/projects/ci-flickr-api/
+ * @version		Version 0.4.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+$lang['flickr_api_required_config_missing']							= 'Falta parámetro de configuración.';
+$lang['flickr_api_params_error']									= 'Los parámetros pasados no son correctos o falta alguno.';
+$lang['flickr_api_error_updating_cache']							= '¡Error al actualizar %s registro!';
+$lang['flickr_api_error_creating_cache']							= '¡Error al crear %s registro!';
+$lang['flickr_api_invalid_request_format']							= 'Formato de petición (Request) no válido "%s".';
+$lang['flickr_api_invalid_response_format']							= 'Formato de respuesta (Response) no válido "%s".';
+$lang['flickr_api_send_request_error']								= 'Ha habido problemas al enviar ty comando al servidor. Error #%s: "%s"';
+$lang['flickr_api_returned_error']									= 'La API de Flickr API devolvió el siguiente error: #%s: "%s"';
+$lang['flickr_api_photo_missing_org_secret_format']					= 'Faltan los valores de formato o la clave secreta de la imagen original.';
+$lang['flickr_api_photo_size_unknown']								= 'El tamaño "%s" no se reconoce.';
+$lang['flickr_api_'] = '';
+$lang['flickr_api_'] = '';
+
+
+/* End of file flickr_api_lang.php */
+/* Location: ./application/language/spanish/flickr_api_lang.php */
